7e0c5ef1bfdb6c50c090e1f748710984cdfecf6e
Evgeny <ev.al.chern@yandex.ru>
2025-08-16 01:33:22 +0300

    KS-2-1 - Решение

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_2
+
+fun main() {
+    val studentOneResult = 3
+    val studentTwoResult = 4
+    val studentThreeResult = 3
+    val studentFourResult = 5
+
+    val averageScore = (studentOneResult + studentTwoResult + studentThreeResult + studentFourResult) / 4.0
+
+    print("Средний бал - $averageScore")
+}
