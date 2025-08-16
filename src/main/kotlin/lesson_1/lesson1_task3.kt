31f16c72b380b9c136543af75eef67aedaf11a9e
Evgeny <ev.al.chern@yandex.ru>
2025-08-16 20:24:10 +0300

    KS-1-3 - Правки согласно комментарию. Тип переменных изменен на string для вывода значений "09" и "07".

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,16 +2,15 @@ package org.example.lesson_1
 
 fun main () {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
 
     println("Год вылета - $year")
     println("Час вылета - $hour")
     println("Минута вылета - $minute")
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
-    print("Время посадки - ")
-    println("$hour:$minute")
+    print("Время посадки - $hour:$minute")
 }
