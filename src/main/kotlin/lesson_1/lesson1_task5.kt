5cf643eebb26c644c0b33c89b1ef1f9de9ef6dd6
Evgeny <ev.al.chern@yandex.ru>
2025-08-16 21:26:38 +0300

    KS-1-4 - Правки согласно комментариям. Перевел магические числа в константы, количество секунд оставил (все же мы можем посчитать и что-то другое), но сделал переменную более читаемой. Переписал вывод с использованием функции stringFormat.

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,13 +1,21 @@
 package org.example.lesson_1
 
+const val secondsInOneMinute: Int = 60
+const val minutesInOneHour: Int = 60
+
 fun main() {
 
-    val seconds = 6480
+    val secondsForCalc = 6480 // не могу считать абсолютной константой, все же мы можем посчитать что-то еще. Но идею я понял! Переписал название переменной, что бы сделать ее более читаемой.
 
-    val secondsRemains = seconds % 60
-    val minutesInSpace = seconds / 60
-    val minutesRemains = minutesInSpace % 60
-    val hoursInSpace = minutesInSpace / 60
 
-    print("0$hoursInSpace:$minutesRemains:0$secondsRemains") // очень смущает отсутствие проверок, но на данном этапе мы же не используем операторы циклов и сравнений
+    val secondsRemains = secondsForCalc % secondsInOneMinute // получаем остаток от деления общего количества секунд на количество секунд в минуте
+    val minutesInSpace = secondsForCalc / secondsInOneMinute // получаем количество минут проведенных Гагарином в космосе
+    val minutesRemains = minutesInSpace % minutesInOneHour // получаем остаток от деления общего количества минут на количество минут в часе
+    val hoursInSpace = minutesInSpace / minutesInOneHour // получаем количество часов проведенных Гагарином в космосе
+
+    print("%02d".format(hoursInSpace))
+    print(":")
+    print("%02d".format(minutesRemains))
+    print(":")
+    print("%02d".format(secondsRemains)) // Спасибо! Очень нужная функция! Нужно будет погуглить как еще склонять (1 год, 2 года, 5 лет), очень часто встречается.
 }
