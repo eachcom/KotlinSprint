388f94312d8ff035c3346bd516d8120ea93d727b
Evgeny <ev.al.chern@yandex.ru>
2025-08-18 00:39:58 +0300

    KS-2-4 - Создал константу для расчета процента, вынес значение баффа в отдельную переменную, поправил расчет процента баффа.

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,11 +1,12 @@
 package org.example.lesson_2
 
-    const val BAF = 20
+    const val PERCENT_MATH = 100.0
 
 fun main () {
     val crystalOre = 7
     val ironOre = 11
-    val bafPercent: Double = BAF/100.0
+    val baf = 20
+    val bafPercent: Double = baf / PERCENT_MATH
 
     val crystalOreBaf = crystalOre*bafPercent
     val ironOreBaf = ironOre*bafPercent
