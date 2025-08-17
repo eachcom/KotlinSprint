3b25546a4c6790ab536f2d56011f04e2e51cea8f
Evgeny <ev.al.chern@yandex.ru>
2025-08-17 23:35:07 +0300

    KS-2-3 - Решение задачи 3 из урока 2

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_2
+
+const val MINUTES_IN_HOUR = 60
+
+fun main () {
+    val hourOfDepart = 9
+    val minuteOfDepart = 39
+    val minutesOnTheWay = 467
+
+    val minutesOfDepartFromMidnight = hourOfDepart*MINUTES_IN_HOUR + minuteOfDepart
+    val minutesOfArrivalFromMidnight = minutesOfDepartFromMidnight + minutesOnTheWay
+
+    val hourOfArrival = minutesOfArrivalFromMidnight / MINUTES_IN_HOUR
+    val minuteOfArrival = minutesOfArrivalFromMidnight % MINUTES_IN_HOUR
+
+    println("%02d:%02d".format(hourOfArrival, minuteOfArrival))
+}
