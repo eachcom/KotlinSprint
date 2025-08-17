a3eb240ded457ba5435a7c9e3f8be9908f06ebfb
Evgeny <ev.al.chern@yandex.ru>
2025-08-17 22:51:34 +0300

    KS-2-1 - Количество учеников вынесено в константу. Изменен тип переменных с оценками на Double

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,12 +1,14 @@
 package org.example.lesson_2
 
-fun main() {
-    val studentOneResult = 3
-    val studentTwoResult = 4
-    val studentThreeResult = 3
-    val studentFourResult = 5
+const val STUDENT_COUNT = 4
 
-    val averageScore = (studentOneResult + studentTwoResult + studentThreeResult + studentFourResult) / 4.0
+fun main() {
+    val studentOneResult = 3.0
+    val studentTwoResult = 4.0
+    val studentThreeResult = 3.0
+    val studentFourResult = 5.0
+
+    val averageScore = (studentOneResult + studentTwoResult + studentThreeResult + studentFourResult) / STUDENT_COUNT
 
     print("Средний бал - $averageScore")
 }
