655f7b407fa1690e59afcd5485cae6f49feba17f
Evgeny <ev.al.chern@yandex.ru>
2025-08-18 01:11:55 +0300

    KS-3-1 - Решение задачи

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main() {
+    var hiMessage = "Доброе утро"
+    val userName = "Иванов Иван"
+
+    println("$hiMessage, $userName!")
+    hiMessage = "Добрый вечер"
+    println("$hiMessage, $userName!")
+}
