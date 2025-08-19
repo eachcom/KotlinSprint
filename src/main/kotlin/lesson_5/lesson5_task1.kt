afb7e0d75181e031de0b0079d181d9d0f6bf2ccb
Evgeny <ev.al.chern@yandex.ru>
2025-08-19 23:36:07 +0300

    KS-5-1 - отделил объявление переменных от логических блоков программы

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -4,6 +4,7 @@ fun main() {
     val firstNumberToCalc = (0..10).random()
     val secondNumberToCalc = (0..10).random()
     val trueAnswer = firstNumberToCalc + secondNumberToCalc
+
     print("Для входа в программу решите пример: $firstNumberToCalc + $secondNumberToCalc = ")
     val userAnswer = readln()
     val userAnswerInt = userAnswer.toInt()
