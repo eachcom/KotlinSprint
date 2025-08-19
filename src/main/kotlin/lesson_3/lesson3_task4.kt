7237f4cf9e786ba2127a6488bd4aace92fb08573
Evgeny <ev.al.chern@yandex.ru>
2025-08-19 10:40:12 +0300

    KS-3-4 - Решение задачи 3 из урока 4

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    var start = "E2"
+    var finish = "E4"
+    var strokeNumber = 1
+
+    println("$start-$finish;$strokeNumber")
+
+    start = "D2"
+    finish = "D3"
+    strokeNumber += 2
+
+    println("$start-$finish;$strokeNumber")
+}
