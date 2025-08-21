0e2b0ae20ca13b2cbf8ac5a47d2a62e0e569210c
Evgeny <ev.al.chern@yandex.ru>
2025-08-21 12:32:56 +0300

    KS-4-4 - создал флаг для проверки четности для и перенес вывод значения в строку вывода данных

--- src/main/kotlin/lesson_4/lesson4-task4.kt
+++ src/main/kotlin/lesson_4/lesson4-task4.kt
@@ -1,17 +1,13 @@
 package org.example.lesson_4
 
 fun main() {
-    val trainingDay = 6
-
-    val exerciseForHand: Boolean = trainingDay % 2 != 0
-    val exerciseForPress: Boolean = trainingDay % 2 != 0
-    val exerciseForBack: Boolean = trainingDay % 2 == 0
-    val exerciseForLeg: Boolean = trainingDay % 2 == 0
+    val trainingDay = 5
+    val isEven = trainingDay % 2 == 0
 
     println("""
-    Упражнения для рук:      $exerciseForHand
-    Упражнения для ног:      $exerciseForPress
-    Упражнения для спины:    $exerciseForBack
-    Упражнения для пресса:   $exerciseForLeg
+    Упражнения для рук:      ${!isEven}
+    Упражнения для ног:      ${!isEven}
+    Упражнения для спины:    $isEven
+    Упражнения для пресса:   $isEven
     """.trimIndent())
 }
