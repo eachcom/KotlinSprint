0c63b82a4745560c77a49d068174891adb5ad8fd
Evgeny <ev.al.chern@yandex.ru>
2025-08-21 17:16:48 +0300

    KS-5-1 - поправил названия переменных

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,13 +1,13 @@
 package org.example.lesson_5
 
 fun main() {
-    val Number1 = (0..10).random()
-    val Number2 = (0..10).random()
+    val number1 = (0..10).random()
+    val number2 = (0..10).random()
 
-    print("Для входа в программу решите пример: $Number1 + $Number2 = ")
+    print("Для входа в программу решите пример: $number1 + $number2 = ")
     val userAnswer = readln().toInt()
 
-    if (userAnswer == Number1 + Number2) {
+    if (userAnswer == number1 + number2) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен.")
