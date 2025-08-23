8952905ee28b1afd03eaa565370f30b18cde032d
Evgeny <ev.al.chern@yandex.ru>
2025-08-23 11:14:31 +0300

    KS-5-5 - Освоил Ctrl + Alt + L, поменял count на size

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -4,20 +4,20 @@ fun main() {
     val userAnswers = mutableSetOf<Int>()
     val trueAnswers = mutableSetOf<Int>()
 
-    for (i in 1..3 ) {
+    for (i in 1..3) {
         trueAnswers.add((0..42).random())
     }
 
     println("Добро пожаловать в игру! Угадайте три числа от 0 до 42!")
 
-    for (i in 1..3 ) {
+    for (i in 1..3) {
         print("Введите число №$i: ")
         userAnswers.add(readln().toInt())
     }
 
     val result = trueAnswers.intersect(userAnswers)
 
-    when (result.count()) {
+    when (result.size) {
         3 -> println("Вы выйграли Джекпот!")
         2 -> println("Вы выйграли большой приз!")
         1 -> println("Вы выйграли маленький приз!")
